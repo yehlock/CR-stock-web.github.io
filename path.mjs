4840fd82199e2f7e3e74b2258700c632b456e483
Emery Lee <leechengyeh02@gmail.com>
2022-10-03 14:41:37 +0800

    fix url

--- path.mjs
+++ path.mjs
@@ -6,7 +6,7 @@ let serverURL;
 if(window.location.href.includes("127.0.0.1")){
     console.log(window.location.href);
     indexURL = "http://127.0.0.1:5500/index.html";
-    revenueURL = "http://127.0.0.1:5500/revenue.html";
+    revenueURL = "http://127.0.0.1:5500/Revenue.html";
     profileURL = "http://127.0.0.1:5500/profile.html";
     serverURL = "https://localhost:7203/Revenue";
     //serverURL = "https://codereview06.azurewebsites.net/api/CodeReviewDb"
@@ -15,7 +15,7 @@ else{
     //     github
     console.log(window.location.href);
     indexURL = "https://yehlock.github.io/CR-stock-web.github.io/";
-    revenueURL = "https://yehlock.github.io/CR-stock-web.github.io/revenue";
+    revenueURL = "https://yehlock.github.io/CR-stock-web.github.io/Revenue";
     profileURL = "https://yehlock.github.io/CR-stock-web.github.io/profile";
     serverURL = "https://stocksserver20220929144022.azurewebsites.net/Revenue";
 }
